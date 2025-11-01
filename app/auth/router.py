75eca126e6cef10e7629f9d7b5d3f3cebe41df6f
fitratgulmamadov <fitratgulmamadov7@gmail.com>
2025-11-01 11:47:10 +0500

    -f

--- app/auth/router.py
+++ app/auth/router.py
@@ -12,7 +12,8 @@ router = APIRouter()
 
 @router.post("/auth/otp")
 async def get_auth_otp(request: Request,  db: Session = Depends(get_db)):
-    print('------->', request.query_params, (await request.body())['phone'])
+    raw_body = await request.body()
+    print("RAW BODY:", raw_body)
     return auth_otp(
         msisdn='992777003304',
         db=db
