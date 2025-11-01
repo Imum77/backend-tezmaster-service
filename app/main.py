288db744e0f69e0ac299a8e1eab6388f240290de
fitratgulmamadov <fitratgulmamadov7@gmail.com>
2025-11-01 11:22:13 +0500

    Add prefix /api/v3

--- app/main.py
+++ app/main.py
@@ -6,9 +6,9 @@ from teznet.router  import router as teznet_router
 app = FastAPI(
     title="GPON API",
     description="API для получения информации о пользователях GPON",
-    version="1.0.0"
+    version="1.0.0",
 )
 
-app.include_router(auth_router, tags=["auth"])
-app.include_router(teznet_router, tags=["cab"])
+app.include_router(auth_router, tags=["auth"]  , prefix='/api/v3')
+app.include_router(teznet_router, tags=["cab"] , prefix='/api/v3')
 
